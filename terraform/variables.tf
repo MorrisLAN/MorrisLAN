d2cc195a545a580565fd2921b579742345c9481f
Maxwell Morris <48025321+m4xmorris@users.noreply.github.com>
2023-11-05 19:28:37 +0000

    Variables tidy up

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,8 +1,52 @@
-# Values set by Terraform Cloud
-variable "cloudflare_account_id" { sensitive = true }
-variable "cloudflare_token" { sensitive = true }
-variable "cloudflare_github_client_id" { sensitive = true }
-variable "cloudflare_github_client_secret" { sensitive = true }
-variable "cloudflare_clancy_secret" { sensitive = true }
-variable "cloudflare_zone_id" {}
-variable "allow_home_assistant_list" { type = list(object({ email = list(string), github = map(string) })) }
+variable "cloudflare_account_id" {
+    type = string
+    sensitive = true
+    description = "Cloudflare Account ID"
+}
+
+variable "cloudflare_token" {
+    type = string
+    sensitive = true
+    description = "Cloudflare API Token"
+}
+
+variable "cloudflare_github_client_id" {
+    type = string
+    sensitive = true
+    description = "GitHub Client ID for SSO"
+}
+
+variable "cloudflare_github_client_secret" {
+    type = string
+    sensitive = true
+    description = "GitHub Client Secret for SSO"
+}
+
+variable "cloudflare_clancy_secret" {
+  type        = string
+  sensitive   = true
+  description = "Cloudflare Tunnel password"
+
+  validation {
+    condition     = can(regex("^[A-Za-z0-9+/]*={0,2}$", var.cloudflare_clancy_secret))
+    error_message = "The value must be a base64-encoded string."
+  }
+
+  validation {
+    condition     = length(var.cloudflare_clancy_secret) >= 32
+    error_message = "The value must be 32 or more bytes in length."
+  }
+}
+
+variable "cloudflare_zone_id" {
+    type = string
+    description = "Cloudflare Zone ID"
+}
+
+variable "allow_home_assistant_list" {
+  type = list(object({
+    email = list(string),
+    github = map(string)
+  }))
+  description = "List of access conditions"
+}
