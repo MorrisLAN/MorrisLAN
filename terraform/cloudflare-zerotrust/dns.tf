5452037c8521048c67ac4581417d6ccfa85987b1
Maxwell Morris <48025321+m4xmorris@users.noreply.github.com>
2023-11-24 18:59:27 +0000

    Added comments to DNS records

--- terraform/cloudflare-zerotrust/dns.tf
+++ terraform/cloudflare-zerotrust/dns.tf
@@ -1,6 +1,7 @@
 resource "cloudflare_record" "access_app_status" {
   zone_id         = var.cloudflare_zone_id
   name            = "status.morrislan.net"
+  comment         = "CNAME to Clancy ZT Tunnel for Network Status page"
   value           = cloudflare_tunnel.clancy.cname
   allow_overwrite = true
   type            = "CNAME"
@@ -11,6 +12,7 @@ resource "cloudflare_record" "access_app_status" {
 resource "cloudflare_record" "access_app_ha" {
   zone_id         = var.cloudflare_zone_id
   name            = "ha.morrislan.net"
+  comment         = "CNAME to Clancy ZT Tunnel for Home Assistant"
   value           = cloudflare_tunnel.clancy.cname
   allow_overwrite = true
   type            = "CNAME"
@@ -21,6 +23,7 @@ resource "cloudflare_record" "access_app_ha" {
 resource "cloudflare_record" "access_app_unifi" {
   zone_id         = var.cloudflare_zone_id
   name            = "unifi.morrislan.net"
+  comment         = "CNAME to Clancy ZT Tunnel for UniFi Controller"
   value           = cloudflare_tunnel.clancy.cname
   allow_overwrite = true
   type            = "CNAME"
@@ -31,6 +34,7 @@ resource "cloudflare_record" "access_app_unifi" {
 resource "cloudflare_record" "access_app_clancyadmin" {
   zone_id         = var.cloudflare_zone_id
   name            = "clancyadmin.morrislan.net"
+  comment         = "CNAME to Clancy ZT Tunnel for Clancy Admin"
   value           = cloudflare_tunnel.clancy.cname
   allow_overwrite = true
   type            = "CNAME"
@@ -41,6 +45,7 @@ resource "cloudflare_record" "access_app_clancyadmin" {
 resource "cloudflare_record" "access_app_homeradmin" {
   zone_id         = var.cloudflare_zone_id
   name            = "homeradmin.morrislan.net"
+  comment         = "CNAME to Clancy ZT Tunnel for Homer Admin"
   value           = cloudflare_tunnel.clancy.cname
   allow_overwrite = true
   type            = "CNAME"
