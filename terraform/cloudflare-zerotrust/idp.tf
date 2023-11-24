7a5c3fd9985771575b4c752d2faa2a74c14b95e4
Maxwell Morris <48025321+m4xmorris@users.noreply.github.com>
2023-11-24 18:27:58 +0000

    Rename GitHub IDP
    Rename GitHub IDP in Cloudflare Zero Trust.

--- terraform/cloudflare-zerotrust/idp.tf
+++ terraform/cloudflare-zerotrust/idp.tf
@@ -1,6 +1,6 @@
 resource "cloudflare_access_identity_provider" "github" {
   account_id = var.cloudflare_account_id
-  name       = "GitHub"
+  name       = "MorrisLAN GitHub"
   type       = "github"
   config {
     client_id     = var.github_client_id
