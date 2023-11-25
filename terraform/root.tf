73e5e34e1ca84fe084f45bbb2146bd6672cdcce6
Maxwell Morris <48025321+m4xmorris@users.noreply.github.com>
2023-11-25 13:06:45 +0000

    Update root.tf

--- terraform/root.tf
+++ terraform/root.tf
@@ -7,15 +7,15 @@ terraform {
 }
 
 provider "cloudflare" {
-  api_token = "${{ secrets.CLOUDFLARE_TOKEN }}"
+  api_token = ${{ secrets.CLOUDFLARE_TOKEN }}
 }
 
 module "cloudflare-zerotrust" {
   source                          = "./cloudflare-zerotrust"
-  cloudflare_token                = "${{ secrets.CLOUDFLARE_TOKEN }}"
-  cloudflare_account_id           = "${{ secrets.CLOUDFLARE_ACCOUNT_ID }}"
-  access_tunnel_clancy_password   = "${{ secrets.ACCESS_TUNNEL_CLANCY_PASSWORD }}"
-  github_client_id     = "${{ secrets.GH_CLIENT_ID }}"
-  github_client_secret = "${{ secrets.CH_CLIENT_SECRET }}"
-  cloudflare_zone_id              = "${{ env.CLOUDFLARE_ZONE_ID }}"
+  cloudflare_token                = ${{ secrets.CLOUDFLARE_TOKEN }}
+  cloudflare_account_id           = ${{ secrets.CLOUDFLARE_ACCOUNT_ID }}
+  access_tunnel_clancy_password   = ${{ secrets.ACCESS_TUNNEL_CLANCY_PASSWORD }}
+  github_client_id     = ${{ secrets.GH_CLIENT_ID }}
+  github_client_secret = ${{ secrets.CH_CLIENT_SECRET }}
+  cloudflare_zone_id              = ${{ env.CLOUDFLARE_ZONE_ID }}
 }
