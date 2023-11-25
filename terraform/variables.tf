e99dd216783cc77f023a4ac866263f65d7e5528c
Maxwell Morris <48025321+m4xmorris@users.noreply.github.com>
2023-11-25 14:57:42 +0000

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,28 +1,34 @@
 variable "cloudflare_token" {
   description = "Cloudflare API Token"
+  type        = string
   sensitive   = true
 }
 
 variable "cloudflare_account_id" {
   description = "Cloudflare Account ID"
+  type        = string
   sensitive   = true
 }
 
 variable "cloudflare_zone_id" {
   description = "Cloudflare Zone ID"
+  type        = string
 }
 
 variable "access_tunnel_clancy_password" {
   description = "Password for Clancy's Access Tunnel"
+  type        = string
   sensitive   = true
 }
 
 variable "gh_client_id" {
   description = "GitHub Client ID"
+  type        = string
   sensitive   = true
 }
 
 variable "gh_client_secret" {
   description = "GitHub Client Secret"
+  type        = string
   sensitive   = true
 }
