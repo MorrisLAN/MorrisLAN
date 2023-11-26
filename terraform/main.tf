9477f66c6c6da6bb6fd070d86817341df0d36d25
Maxwell Morris <git@maxmorris.io>
2023-11-26 02:21:24 +0000

    Correct domain

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,7 +12,7 @@ module "cloudflare-zerotrust" {
 resource "cloudflare_record" "github_verify" {
   count           = var.env == "prod" ? 1 : 0
   zone_id         = var.cloudflare_zone_id
-  name            = "_github-challenge-MorrisLAN-org"
+  name            = "_github-challenge-MorrisLAN-org.morrislan.net"
   comment         = "Domain ownership verification for GitHub"
   value           = "e2628155b4"
   allow_overwrite = true
