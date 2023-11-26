2ed2b1a0e0f77f39b2f178e828199a71f39c1dec
Maxwell Morris <git@maxmorris.io>
2023-11-26 02:18:02 +0000

    Add GitHub verification DNS record

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,3 +8,13 @@ module "cloudflare-zerotrust" {
   github_client_secret          = var.gh_client_secret
   cloudflare_zone_id            = var.cloudflare_zone_id
 }
+
+resource "cloudflare_record" "github_verify" {
+  count           = var.env == "prod" ? 1 : 0
+  zone_id         = var.cloudflare_zone_id
+  name            = "_github-challenge-MorrisLAN-org"
+  comment         = "Domain ownership verification for GitHub"
+  value           = "e2628155b4"
+  allow_overwrite = true
+  type            = "TXT"
+}
