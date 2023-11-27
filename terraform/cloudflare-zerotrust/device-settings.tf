a1cf73e848aeeff314243bf4972a6831023cf767
Maxwell Morris <48025321+m4xmorris@users.noreply.github.com>
2023-11-27 18:09:05 +0000

    Tweaking device settings

--- terraform/cloudflare-zerotrust/device-settings.tf
+++ terraform/cloudflare-zerotrust/device-settings.tf
@@ -8,7 +8,7 @@ resource "cloudflare_device_settings_policy" "morrislan_settings" {
   switch_locked        = true
   allowed_to_leave     = true
   allow_updates        = true
-  auto_connect         = 1
+  auto_connect         = 5
   service_mode_v2_mode = "warp"
   service_mode_v2_port = 3000
 }
