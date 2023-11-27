864e7ebd93f626a80400cdae8bf6d69a842e65bc
Maxwell Morris <git@maxmorris.io>
2023-11-27 16:09:17 +0000

    Update to patched module

--- terraform/cloudflare-zerotrust/applications.tf
+++ terraform/cloudflare-zerotrust/applications.tf
@@ -1,6 +1,6 @@
 module "access_app_status" {
   source                = "m4xmorris/access-application/cloudflare"
-  version               = "3.1.0"
+  version               = "3.1.1"
   name                  = "Network Status (${var.env})"
   logo                  = "https://static-00.iconduck.com/assets.00/uptime-kuma-icon-512x469-ce3ut52z.png"
   domain                = "status-${var.env}.morrislan.net"
@@ -13,13 +13,11 @@ module "access_app_status" {
   cloudflare_zone_id    = var.cloudflare_zone_id
   github_idp            = cloudflare_access_identity_provider.github.id
   github_org            = "MorrisLAN"
-  github_teams          = []
-  allowed_emails        = []
 }
 
 module "access_app_ha" {
   source                = "m4xmorris/access-application/cloudflare"
-  version               = "3.1.0"
+  version               = "3.1.1"
   name                  = "Home Assistant (${var.env})"
   logo                  = "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6e/Home_Assistant_Logo.svg/2048px-Home_Assistant_Logo.svg.png"
   domain                = "ha-${var.env}.morrislan.net"
@@ -32,13 +30,11 @@ module "access_app_ha" {
   cloudflare_zone_id    = var.cloudflare_zone_id
   github_idp            = cloudflare_access_identity_provider.github.id
   github_org            = "MorrisLAN"
-  github_teams          = []
-  allowed_emails        = []
 }
 
 module "access_app_unifi" {
   source                = "m4xmorris/access-application/cloudflare"
-  version               = "3.1.0"
+  version               = "3.1.1"
   name                  = "UniFi (${var.env})"
   logo                  = "https://assets-global.website-files.com/622b70d8906c7ab0c03f77f8/63b40a92093c6b2f3767e4e6_tMCv8T-y_400x400.png"
   domain                = "unifi-${var.env}.morrislan.net"
@@ -52,14 +48,12 @@ module "access_app_unifi" {
   github_idp            = cloudflare_access_identity_provider.github.id
   github_org            = "MorrisLAN"
   github_teams          = ["Admins"]
-  allowed_emails        = []
-  device_policy_mode    = "require"
   device_policy_rules   = local.all_device_rules
 }
 
 module "access_app_clancyadmin" {
   source                = "m4xmorris/access-application/cloudflare"
-  version               = "3.1.0"
+  version               = "3.1.1"
   name                  = "Clancy Admin (${var.env})"
   logo                  = "https://user-images.githubusercontent.com/23664304/36225509-be167464-117f-11e8-9bfc-cba7acd889c5.png"
   domain                = "clancyadmin-${var.env}.morrislan.net"
@@ -73,14 +67,12 @@ module "access_app_clancyadmin" {
   github_idp            = cloudflare_access_identity_provider.github.id
   github_org            = "MorrisLAN"
   github_teams          = ["Admins"]
-  allowed_emails        = []
-  device_policy_mode    = "require"
   device_policy_rules   = local.all_device_rules
 }
 
 module "access_app_homeradmin" {
   source                = "m4xmorris/access-application/cloudflare"
-  version               = "3.1.0"
+  version               = "3.1.1"
   name                  = "Homer Admin (${var.env})"
   logo                  = "https://tweakers.net/ext/i/2005515460.png"
   domain                = "homeradmin-${var.env}.morrislan.net"
@@ -94,7 +86,5 @@ module "access_app_homeradmin" {
   github_idp            = cloudflare_access_identity_provider.github.id
   github_org            = "MorrisLAN"
   github_teams          = ["Admins"]
-  allowed_emails        = []
-  device_policy_mode    = "require"
   device_policy_rules   = local.all_device_rules
 }
