5d93827bdb697df885e7d77043922c4d5b4b19ef
Maxwell Morris <git@maxmorris.io>
2023-11-28 21:22:07 +0000

    Various tweaks

--- terraform/cloudflare-zerotrust/device-settings.tf
+++ terraform/cloudflare-zerotrust/device-settings.tf
@@ -6,9 +6,25 @@ resource "cloudflare_device_settings_policy" "morrislan_settings" {
   enabled              = true
   allow_mode_switch    = false
   switch_locked        = true
+  allowed_to_leave     = false
+  allow_updates        = true
+  auto_connect         = 120
+  service_mode_v2_mode = "warp"
+  service_mode_v2_port = 3000
+}
+
+resource "cloudflare_device_settings_policy" "home_settings" {
+  account_id           = var.cloudflare_account_id
+  name                 = "On Home Network (Disabled)"
+  description          = "Connected to Home Network - Posture Checks Only"
+  precedence           = 10
+  match                = "network == ${cloudflare_device_managed_networks.home.id}"
+  enabled              = true
+  allow_mode_switch    = true
+  switch_locked        = true
   allowed_to_leave     = true
   allow_updates        = true
-  auto_connect         = 5
-  service_mode_v2_mode = "warp"
+  auto_connect         = 300
+  service_mode_v2_mode = "posture_onlyxs"
   service_mode_v2_port = 3000
 }
