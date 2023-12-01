80865c6212c8547b722f51c785a8e8e3487047ea
Maxwell Morris <git@maxmorris.io>
2023-12-01 19:52:49 +0000

    `terraform fmt`

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -4,6 +4,6 @@ output "tunnel_token" {
 }
 
 output "home_doh" {
-  value = cloudflare_teams_location.home.doh_subdomain
+  value     = cloudflare_teams_location.home.doh_subdomain
   sensitive = true
 }
