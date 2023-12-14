20872a112201f4f05bb959c8519ae6ce22933776
Maxwell Morris <git@maxmorris.io>
2023-12-14 14:40:13 +0000

    Add static_site module

--- terraform/modules.tf
+++ terraform/modules.tf
@@ -1,3 +1,28 @@
+module "static_site" {
+  source      = "m4xmorris/static-site/digitalocean"
+  version     = "1.3.1"
+  site_name   = "morrislan"
+  description = "Webroot for MorrisLAN"
+  environment = "Production"
+  region      = "lon"
+  domain      = "morrislan.net"
+  source_repo = "MorrisLAN/morrislan"
+  source_branches = {
+    "main" = "/"
+  }
+  source_dir         = "http"
+  output_dir         = "."
+  build_command      = "echo 'Static HTML, no build required.'"
+  manage_dns         = true
+  cloudflare_zone_id = var.cloudflare_zone_id
+  alert_policy = [
+    "DOMAIN_LIVE",
+    "DEPLOYMENT_LIVE",
+    "DOMAIN_FAILED",
+    "DEPLOYMENT_FAILED"
+  ]
+}
+
 module "cloudflare-zerotrust" {
   source                = "./cloudflare-zerotrust"
   cloudflare_token      = var.cloudflare_token
