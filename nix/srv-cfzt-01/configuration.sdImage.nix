c8d0c611ede5612b7a6e5c04ffc2e44e36eb93d3
Maxwell Morris <git@maxmorris.io>
2023-12-21 19:01:31 +0000

    First attempt to build srv-cfzt-01 image

---
+++ nix/srv-cfzt-01/configuration.sdImage.nix
@@ -0,0 +1,78 @@
+{ config, pkgs, lib, ... }:
+{
+
+  imports = [
+    <nixpkgs/nixos/modules/installer/cd-dvd/sd-image-aarch64.nix>
+    <nixpkgs/nixos/modules/installer/cd-dvd/channel.nix>
+  ];
+
+  sdImage.compressImage = false;
+  
+  boot.loader.grub.enable = false;
+  boot.loader.generic-extlinux-compatible.enable = true;
+ 
+  boot.kernelPackages = pkgs.linuxPackages_5_4;
+
+  environment.systemPackages = with pkgs; [ vim curl wget nano neofetch cloudflared libraspberrypi raspberrypi-eeprom ];
+
+  services.openssh = {
+      enable = true;
+      ports = [ 4022 ];
+      permitRootLogin = "no";
+      passwordAuthentication = false;
+  };
+
+  programs.zsh = {
+      enable = true;
+      ohMyZsh = {
+          enable = true;
+          theme = "af-magic";
+      };
+  };
+
+  networking.firewall.enable = true;
+  networking.firewall.allowedTCPPorts = [ 4022 ];
+  networking = {
+    hostName = "srv-cfzt-01";
+    interfaces.eth0 = {
+      useDHCP = false;
+      ipv4.addresses = [{
+       address = "10.1.240.5";
+       rand = "asd";
+       prefixLength = 24;
+      }];
+      defaultGateway = "10.1.240.1";
+      nameservers = [ "10.1.240.3" ];
+    };
+  };
+
+  time.timeZone = "Europe/London";
+  i18n.defaultLocale = "en_GB.UTF-8";
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_GB.UTF-8";
+    LC_IDENTIFICATION = "en_GB.UTF-8";
+    LC_MEASUREMENT = "en_GB.UTF-8";
+    LC_MONETARY = "en_GB.UTF-8";
+    LC_NAME = "en_GB.UTF-8";
+    LC_NUMERIC = "en_GB.UTF-8";
+    LC_PAPER = "en_GB.UTF-8";
+    LC_TELEPHONE = "en_GB.UTF-8";
+    LC_TIME = "en_GB.UTF-8";
+  };
+
+  users.defaultUserShell = pkgs.zsh;
+  users.mutableUsers = false;
+  users.users = {
+    admin = {
+      uid = 1000;
+      home = "/home/admin";
+      name = "admin";
+      group = "admin";
+      shell = pkgs.zsh;
+      extraGroups = [ "wheel" ];
+    };
+  };
+  users.extraUsers.root.openssh.authorizedKeys.keys = [
+     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHjgkh0LyGVjWzYnCxrKC5dxQMHE3ky7s/vFpAyjfk5l"
+  ];
+}
