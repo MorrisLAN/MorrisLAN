00812d80364afb67dcb53e8852ecf444375a02d2
Maxwell Morris <git@maxmorris.io>
2023-12-21 22:34:45 +0000

    Tweak

--- nix/raspi4.nix
+++ nix/raspi4.nix
@@ -1,36 +1,25 @@
-{ config, pkgs, lib, ... }:
-{
-
+{ config, pkgs, lib, ... }: {
+  nixpkgs.localSystem.system = "aarch64-linux";
   imports = [
-    <nixpkgs/nixos/modules/installer/cd-dvd/sd-image-aarch64.nix>
-    <nixpkgs/nixos/modules/installer/cd-dvd/channel.nix>
+    <nixpkgs/nixos/modules/installer/cd-dvd/sd-image.nix>
+    ./configuration.nix
   ];
 
-  sdImage.compressImage = false;
+  boot = {
+    kernelPackages = pkgs.linuxPackages_rpi4;
+    loader.grub.enable = false;
+    loader.generic-extlinux-compatible.enable = true;
+    consoleLogLevel = lib.mkDefault 7;
+    kernelParams =
+      [ "console=ttyS0,115200n8" "console=ttyAMA0,115200n8" "console=tty0" ];
+    initrd.availableKernelModules = [
+      "vc4"
+      "bcm2835_dma"
+      "i2c_bcm2835"
+    ];
+  };
   
-  boot.loader.grub.enable = false;
-  boot.loader.generic-extlinux-compatible.enable = true;
-
-  boot.kernelPackages = pkgs.linuxPackages_5_4;
-
-  nixpkgs.config.allowUnfree = true;
-  environment.systemPackages = with pkgs; [ vim curl wget nano neofetch ];
-
   services.openssh.enable = true;
-
-  programs.zsh = {
-      enable = true;
-      ohMyZsh = {
-          enable = true;
-          theme = "af-magic";
-      };
-  };
-
-  networking.firewall.enable = false;
-  networking.networkmanager = {
-      enable = true;
-      unmanaged = [ "wlan0" ];
-  };
 
   time.timeZone = "Europe/London";
   i18n.defaultLocale = "en_GB.UTF-8";
@@ -61,4 +50,33 @@
       ];
     };
   };
+
+  sdImage = {
+    populateFirmwareCommands = let
+      configTxt = pkgs.writeText "config.txt" ''
+        [pi3]
+        kernel=u-boot-rpi3.bin
+        [pi4]
+        kernel=u-boot-rpi4.bin
+        enable_gic=1
+        armstub=armstub8-gic.bin
+        [all]
+        arm_64bit=1
+        enable_uart=1
+        avoid_warnings=1
+      '';
+    in ''
+      (cd ${pkgs.raspberrypifw}/share/raspberrypi/boot && cp bootcode.bin fixup*.dat start*.elf $NIX_BUILD_TOP/firmware/)
+      cp ${configTxt} firmware/config.txt
+      cp ${pkgs.ubootRaspberryPi3_64bit}/u-boot.bin firmware/u-boot-rpi3.bin
+      cp ${pkgs.ubootRaspberryPi4_64bit}/u-boot.bin firmware/u-boot-rpi4.bin
+      cp ${pkgs.raspberrypi-armstubs}/armstub8-gic.bin firmware/armstub8-gic.bin
+      cp ${pkgs.raspberrypifw}/share/raspberrypi/boot/bcm2711-rpi-4-b.dtb firmware/
+    '';
+    populateRootCommands = ''
+      mkdir -p ./files/boot
+      ${config.boot.loader.generic-extlinux-compatible.populateCmd} -c ${config.system.build.toplevel} -d ./files/boot
+    '';
+  };
+
 }
