6c581d6bf44591dd519ea2a0667c462b0fc1c8ad
Maxwell Morris <git@maxmorris.io>
2023-12-22 21:40:44 +0000

    Tidy up

--- nix/srv-cfzt-01/configuration.nix
+++ nix/srv-cfzt-01/configuration.nix
@@ -1,4 +1,8 @@
 { config, pkgs, lib, ... }: {
+  imports = [
+    ../base.nix
+  ];
+
   fileSystems = {
     "/" = {
       device = "/dev/disk/by-label/NIXOS_SD";
@@ -7,22 +11,7 @@
   };
   swapDevices = [ { device = "/swapfile"; size = 1024; } ];
 
-  nixpkgs.config.allowUnfree = true;
-  environment.systemPackages = with pkgs; [ vim curl wget nano neofetch cloudflared ];
-
-  services.openssh = {
-      enable = true;
-      ports = [ 4022 ];
-      passwordAuthentication = false;
-  };
-
-  programs.zsh = {
-      enable = true;
-      ohMyZsh = {
-          enable = true;
-          theme = "af-magic";
-      };
-  };
+  environment.systemPackages = with pkgs; [ cloudflared ];
 
   networking.firewall.enable = true;
   networking.firewall.allowedTCPPorts = [ 4022 ];
@@ -39,44 +28,15 @@
     nameservers = [ "10.1.240.3" ];
   };
 
-  time.timeZone = "Europe/London";
-  i18n.defaultLocale = "en_GB.UTF-8";
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_GB.UTF-8";
-    LC_IDENTIFICATION = "en_GB.UTF-8";
-    LC_MEASUREMENT = "en_GB.UTF-8";
-    LC_MONETARY = "en_GB.UTF-8";
-    LC_NAME = "en_GB.UTF-8";
-    LC_NUMERIC = "en_GB.UTF-8";
-    LC_PAPER = "en_GB.UTF-8";
-    LC_TELEPHONE = "en_GB.UTF-8";
-    LC_TIME = "en_GB.UTF-8";
-  };
-
-  users.defaultUserShell = pkgs.zsh;
-  users.mutableUsers = false;
   users.users = {
-    admin = {
-      uid = 1000;
-      isNormalUser = true;
-      home = "/home/admin";
-      name = "admin";
-      group = "admin";
-      shell = pkgs.zsh;
-      extraGroups = [ "wheel" ];
-      openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHjgkh0LyGVjWzYnCxrKC5dxQMHE3ky7s/vFpAyjfk5l"
-      ];
-    };
     cloudflared = {
       group = "cloudflared";
       isSystemUser = true;
     };
   };
-  users.groups.admin = {};
   users.groups.cloudflared = { };
 
-  systemd.services.morrislan_tunnel = {
+  systemd.services.cloudflared = {
     wantedBy = [ "multi-user.target" ];
     after = [ "network.target" ];
     serviceConfig = {
