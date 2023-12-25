5fe75123dd63350a9ab61fc8261f596da80cb045
Maxwell Morris <git@maxmorris.io>
2023-12-25 12:44:52 +0000

    Test

--- nix/srv-tp-01/configuration.sdImage.nix
+++ nix/srv-tp-01/configuration.sdImage.nix
@@ -1,39 +1,7 @@
 { config, lib, pkgs, ... }: {
-  sdImage.compressImage = false;
   nixpkgs.localSystem.system = "x86_64-linux";
   imports = [
-    <nixpkgs/nixos/modules/installer/sd-card/sd-image.nix>
+    <nixpkgs/nixos/modules/virtualisation/digital-ocean-image.nix>
     ./configuration.nix
   ];
-  boot.kernelPackages = pkgs.linuxPackages;
-  boot.loader.grub.enable = false;
-  boot.loader.generic-extlinux-compatible.enable = true;
-
-  boot.consoleLogLevel = lib.mkDefault 7;
- 
-  boot.kernelParams =
-    [ "console=ttyS0,115200n8" "console=tty0" ];
-
-  boot.initrd.availableKernelModules = [
-    "virtio_pci"
-    "virtio_blk"
-    "virtio_net"
-  ];
-
-  nixpkgs.overlays = [
-    (final: super: {
-      makeModulesClosure = x:
-        super.makeModulesClosure (x // { allowMissing = true; });
-    })
-  ];
-
-  sdImage = {
-    populateFirmwareCommands = ''
-    '';
-    populateRootCommands = ''
-      mkdir -p ./files/boot
-      ${config.boot.loader.generic-extlinux-compatible.populateCmd} -c ${config.system.build.toplevel} -d ./files/boot
-    '';
-  };
-
 }
