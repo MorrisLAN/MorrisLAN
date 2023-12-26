12737ab035b8892773aef7a5b44533b27bec833e
Maxwell Morris <git@maxmorris.io>
2023-12-26 00:40:44 +0000

    Patch

--- terraform/storage.tf
+++ terraform/storage.tf
@@ -1,5 +1,5 @@
 resource "digitalocean_spaces_bucket" "nixos" {
-  name   = var.nixos_bucket
+  name   = "${var.nixos_bucket}-nixos"
   region = "fra1"
   versioning { enabled = "true" }
   acl = "private"
