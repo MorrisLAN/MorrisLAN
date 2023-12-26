7c900dcb3966050311fb982ff661f0a9bb089fa2
Maxwell Morris <git@maxmorris.io>
2023-12-26 23:08:38 +0000

    Test

---
+++ nix/vm-maxmbp.nix
@@ -0,0 +1,53 @@
+{ config, pkgs, lib, ... }: {
+  imports = [
+    <nixpkgs/nixos/modules/profiles/qemu-guest.nix>
+    ./modules/base.nix
+  ];
+
+  boot = {
+    consoleLogLevel = lib.mkDefault 7;
+    loader = {
+      grub.enable = false;
+      generic-extlinux-compatible.enable = true;
+    };
+    kernelParams = [ "console=ttyAMA0,115200n8" "console=tty0" ];
+  };
+
+  nixpkgs = {
+    localSystem.system = "aarch64-linux";
+    overlays = [
+      (final: super: {
+        makeModulesClosure = x:
+          super.makeModulesClosure (x // { allowMissing = true; });
+      })
+    ];
+  };
+
+  fileSystems = {
+    "/" = {
+      device = "/dev/vda2";
+      fsType = "ext4";
+    };
+    "/boot" = {
+      device = "/dev/vda1";
+      fsType = "vfat";
+    };
+  };
+
+  networking = {
+    hostName = "vm-maxmbp";
+    interfaces.enp0s5 = {
+      useDHCP = true;
+    };
+  };
+
+  system.build.image = import <nixpkgs/nixos/lib/make-disk-image.nix> {
+    diskSize = 10000;
+    format = "qcow2-compressed";
+    installBootLoader = true;
+    partitionTableType = "efi";
+    inherit config lib pkgs;
+  };
+
+  system.stateVersion = "23.11";
+}
