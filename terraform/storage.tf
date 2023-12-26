4684c32308dc5202364f9c1b09a1370b039a8e46
Maxwell Morris <git@maxmorris.io>
2023-12-26 00:19:04 +0000

    Format

--- terraform/storage.tf
+++ terraform/storage.tf
@@ -1,6 +1,6 @@
 resource "digitalocean_spaces_bucket" "nixos" {
   name   = "morrislan-nixos-images"
   region = "fra1"
-  versioning {enabled = "true"}
+  versioning { enabled = "true" }
   acl = "private"
 }
--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -18,7 +18,7 @@ provider "cloudflare" {
 }
 
 provider "digitalocean" {
-  token = var.do_token
+  token             = var.do_token
   spaces_access_id  = var.do_spaces_access_key
   spaces_secret_key = var.do_spaces_secret_key
 }
