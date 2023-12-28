64b04ef7d57064cdb36ba5156acbce005c86a91a
Maxwell Morris <48025321+m4xmorris@users.noreply.github.com>
2023-12-28 02:04:30 +0000

    Typo

--- nix/modules/base.nix
+++ nix/modules/base.nix
@@ -7,7 +7,7 @@
     wantedBy = [ "multi-user.target" ];
     after = [ "network.target" ];
     serviceConfig = {
-      Type = simple;
+      Type = "simple";
       Restart = "always";
     };
     script = ''
