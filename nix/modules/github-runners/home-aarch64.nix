c9518b1b5b9a7bb992d5236b3004ab01548398ae
Maxwell Morris <48025321+m4xmorris@users.noreply.github.com>
2023-12-28 00:53:36 +0000

    Update home-aarch64.nix

--- nix/modules/github-runners/home-aarch64.nix
+++ nix/modules/github-runners/home-aarch64.nix
@@ -1,15 +1,32 @@
 { config, pkgs, lib, ... }: {
   environment.systemPackages = with pkgs; [ git github-runner ];
+
+  users.users = {
+    github-runner = {
+      group = "github-runner";
+      isSystemUser = true;
+    };
+  };
+  users.groups.github-runner = { };
+
   environment.etc.gh-runner-token = {
     text = "SECRET_GH_RUNNER_TOKEN";
     mode = "0644";
   };
   services.github-runners = {
     home-aarch64 = {
-      enable = true;
       name = "home-aarch64";
+      enable = true;
+      replace = true;
+      user = "github-runner";
+      workDir = "/build";
       tokenFile = "/etc/gh-runner-token";
       url = "https://github.com/MorrisLAN";
     };
   };
+
+  systemd.tmpfiles.rules = [
+    "d /build 0600 github-runner github-runner -"
+  ];
+
 }
