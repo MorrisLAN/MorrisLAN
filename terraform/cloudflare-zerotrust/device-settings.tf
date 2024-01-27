57a8b3721eacdfcf2ba530ccf69bb2102dbf8a60
Maxwell Morris <48025321+m4xmorris@users.noreply.github.com>
2024-01-27 23:21:11 +0000

    Switch to WARP split tunnel mode

--- terraform/cloudflare-zerotrust/device-settings.tf
+++ terraform/cloudflare-zerotrust/device-settings.tf
@@ -1,26 +1,42 @@
-resource "cloudflare_device_settings_policy" "remote" {
+resource "cloudflare_device_settings_policy" "default" {
   account_id        = var.cloudflare_account_id
-  name              = "Remote Profile"
-  description       = "Remote WARP profile for accessing MorrisLAN resources"
+  name              = "Default"
+  description       = "Default WARP profile for accessing MorrisLAN resources"
   default           = true
   enabled           = true
   allow_mode_switch = false
   switch_locked     = false
   allowed_to_leave  = true
-  allow_updates     = false
-  auto_connect      = 900
+  allow_updates     = true
+  auto_connect      = 2700
 }
 
-resource "cloudflare_device_settings_policy" "home" {
-  account_id        = var.cloudflare_account_id
-  name              = "Home Profile"
-  description       = "Home WARP profile for accessing MorrisLAN resources"
-  precedence        = 1
-  match             = "network == \"Home\""
-  enabled           = true
-  allow_mode_switch = false
-  switch_locked     = false
-  allowed_to_leave  = true
-  allow_updates     = true
-  auto_connect      = 900
+resource "cloudflare_split_tunnel" "cloudflare" {
+  account_id = var.cloudflare_account_id
+  policy_id  = cloudflare_device_settings_policy.default.id
+  mode       = "include"
+  tunnels {
+    host        = "*.cloudflare.com"
+    description = "Cloudflare"
+  }
+}
+
+resource "cloudflare_split_tunnel" "cloudflare_access" {
+  account_id = var.cloudflare_account_id
+  policy_id  = cloudflare_device_settings_policy.default.id
+  mode       = "include"
+  tunnels {
+    host        = "*.cloudflareaccess.com"
+    description = "Cloudflare Access"
+  }
+}
+
+resource "cloudflare_split_tunnel" "morrislan" {
+  account_id = var.cloudflare_account_id
+  policy_id  = cloudflare_device_settings_policy.default.id
+  mode       = "include"
+  tunnels {
+    host        = "*.morrislan.net"
+    description = "MorrisLAN"
+  }
 }
