79a2724876c0628f200d0c4c5699b06f0e32f9f1
Maxwell Morris <git@maxmorris.io>
2024-07-28 22:51:30 +0100

    More fixy

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -6,3 +6,27 @@ resource "cloudflare_record" "github_verify" {
   allow_overwrite = true
   type            = "TXT"
 }
+
+resource "cloudflare_record" "smtpgo_1" {
+  zone_id         = var.cloudflare_zone_id
+  name            = "em569112.morrislan.net"
+  value           = "return.smtp2go.net"
+  allow_overwrite = true
+  type            = "CNAME"
+}
+
+resource "cloudflare_record" "smtpgo_2" {
+  zone_id         = var.cloudflare_zone_id
+  name            = "email-track.morrislan.net"
+  value           = "track.smtp2go.net"
+  allow_overwrite = true
+  type            = "CNAME"
+}
+
+resource "cloudflare_record" "smtpgo_3" {
+  zone_id         = var.cloudflare_zone_id
+  name            = "s569112._domainkey.morrislan.net"
+  value           = "dkim.smtp2go.net"
+  allow_overwrite = true
+  type            = "CNAME"
+}
