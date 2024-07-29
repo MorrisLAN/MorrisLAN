23ef21e944cd407dcc830276da7189f5e76f84e5
Maxwell Morris <git@maxmorris.io>
2024-07-29 01:12:34 +0100

    Fix (maybe)

--- terraform/droplets.tf
+++ terraform/droplets.tf
@@ -1,9 +1,9 @@
 resource "digitalocean_droplet" "pcdo-ch-1" {
-  image  = "161584796" # NixOS 24.05
-  name   = "pcdo-ch-1"
-  region = "lon1"
-  size   = "s-2vcpu-2gb"
-  backups = true
-  ssh_keys = [ "b3:57:07:56:0f:8b:88:eb:43:cd:f1:d7:38:59:a3:b5" ]
+  image             = "161584796" # NixOS 24.05
+  name              = "pcdo-ch-1"
+  region            = "lon1"
+  size              = "s-2vcpu-2gb"
+  backups           = true
+  ssh_keys          = [var.digitalocean_ssh_key]
   graceful_shutdown = true
 }
