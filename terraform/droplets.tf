dec06a6012fd9a79182e3253d55a00b0f46e6363
Maxwell Morris <git@maxmorris.io>
2024-07-29 01:15:51 +0100

    More likely fix?

--- terraform/droplets.tf
+++ terraform/droplets.tf
@@ -1,9 +1,13 @@
+data "digitalocean_ssh_key" "morrislan" {
+  id = "b3:57:07:56:0f:8b:88:eb:43:cd:f1:d7:38:59:a3:b5"  # Replace with the ID of your SSH key
+}
+
 resource "digitalocean_droplet" "pcdo-ch-1" {
-  image             = "161584796" # NixOS 24.05
-  name              = "pcdo-ch-1"
-  region            = "lon1"
-  size              = "s-2vcpu-2gb"
-  backups           = true
-  ssh_keys          = [var.digitalocean_ssh_key]
+  image  = "161584796" # NixOS 24.05
+  name   = "pcdo-ch-1"
+  region = "lon1"
+  size   = "s-2vcpu-2gb"
+  backups = true
+  ssh_keys = [ digitalocean_ssh_key.morrislan.id ]
   graceful_shutdown = true
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -20,8 +20,3 @@ variable "cloudflare_zone_id" {
   description = "Cloudflare Zone ID"
   type        = string
 }
-
-variable "digitalocean_ssh_key" {
-  description = "SSH key used to access DigitalOcean Droplets"
-  default     = "b3:57:07:56:0f:8b:88:eb:43:cd:f1:d7:38:59:a3:b5"
-}
