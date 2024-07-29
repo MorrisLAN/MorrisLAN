8008cdcae51911acfdac97343bb7c6381b7340ac
Maxwell Morris <git@maxmorris.io>
2024-07-29 01:30:50 +0100

    last try before bedtime

--- terraform/droplets.tf
+++ terraform/droplets.tf
@@ -1,6 +1,6 @@
 data "digitalocean_ssh_key" "onepassword" {
   name = "1Password"
-  fingerprint = "b3:57:07:56:0f:8b:88:eb:43:cd:f1:d7:38:59:a3:b5"
+  public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHjgkh0LyGVjWzYnCxrKC5dxQMHE3ky7s/vFpAyjfk5l"
 }
 
 resource "digitalocean_droplet" "pcdo-ch-1" {
