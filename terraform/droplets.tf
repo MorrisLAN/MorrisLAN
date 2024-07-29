a86bb289100bf88a34ca6d781866ab04dd6ff3cd
Maxwell Morris <git@maxmorris.io>
2024-07-29 01:03:46 +0100

    Thats not nix...

--- terraform/droplets.tf
+++ terraform/droplets.tf
@@ -3,7 +3,7 @@ resource "digitalocean_droplet" "pcdo-ch-1" {
   name   = "pcdo-ch-1"
   region = "lon1"
   size   = "s-2vcpu-2gb"
-  backups = true;
+  backups = true
   ssh_keys = [b3:57:07:56:0f:8b:88:eb:43:cd:f1:d7:38:59:a3:b5]
   graceful_shutdown = true;
 }
