f73ee726659de60f5b6913029b13244b80574fe2
Maxwell Morris <git@maxmorris.io>
2024-07-29 01:16:55 +0100

    double woops

--- terraform/droplets.tf
+++ terraform/droplets.tf
@@ -1,5 +1,5 @@
 data "digitalocean_ssh_key" "morrislan" {
-  id = "b3:57:07:56:0f:8b:88:eb:43:cd:f1:d7:38:59:a3:b5"  # Replace with the ID of your SSH key
+  id = "b3:57:07:56:0f:8b:88:eb:43:cd:f1:d7:38:59:a3:b5"
 }
 
 resource "digitalocean_droplet" "pcdo-ch-1" {
