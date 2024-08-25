4d329b94c3897fad6aa3b73cde2f232c744c9c7d
Maxwell Morris <git@maxmorris.io>
2024-08-25 20:40:47 +0100

    deal with the ups later (foreshadowing probs)

--- nix/hardware/dell-r730.nix
+++ nix/hardware/dell-r730.nix
@@ -33,16 +33,6 @@
     };
   };
 
-  power.ups = {
-    enable = true;
-    mode = "netserver";
-    openFirewall = true;
-    ups."officeups" = {
-      driver = "apcsmart";
-      port = "auto";
-    };
-  };
-
   # systemd.services."sync-esp" = {
   #   description = "Synchronize ESP partitions";
   #   after = [ "boot.mount" ];
