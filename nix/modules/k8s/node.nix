053abd84be3768d5a4afd8d935e398b7de0c4da8
Maxwell Morris <git@maxmorris.io>
2024-08-27 23:44:58 +0100

    more fixing in progresss

--- nix/modules/k8s/node.nix
+++ nix/modules/k8s/node.nix
@@ -1,7 +1,11 @@
 { config, pkgs, ... }:
 {
   
-  networking.extraHosts = "10.1.240.5 hc-ch-1.morrislan.net";
+  networking.extraHosts = 
+  ''
+    10.1.240.5 hc-ch-1.morrislan.net
+    10.1.240.5 hc-ch-api.morrislan.net
+  '';
 
   environment.systemPackages = with pkgs; [
     kompose
@@ -11,8 +15,8 @@
 
   services.kubernetes = {
     roles = ["master" "node"];
-    masterAddress = "hc-ch-1.morrislan.net";
-    apiserverAddress = "https://hc-ch-1.morrislan.net:6443";
+    masterAddress = "hc-ch-api.morrislan.net";
+    apiserverAddress = "https://hc-ch-api.morrislan.net:6443";
     easyCerts = true;
     addons.dns.enable = true;
     kubelet.extraOpts = "--fail-swap-on=false";
