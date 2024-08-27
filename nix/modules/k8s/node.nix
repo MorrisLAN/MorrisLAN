9e4472f57199164ce7364e2fa7b1977319d812d8
Maxwell Morris <git@maxmorris.io>
2024-08-27 23:15:47 +0100

    fixed (i think?)??

--- nix/modules/k8s/node.nix
+++ nix/modules/k8s/node.nix
@@ -1,19 +1,24 @@
-{ config, pkgs, lib, ... }: {
-  nixpkgs.config.allowUnfree = true;
+{ config, pkgs, ... }:
+{
+  
+  networking.extraHosts = "10.1.240.5 hc-ch-1.morrislan.net";
 
-  environment = {
-    systemPackages = with pkgs; [ kubernetes kubectl ];
-  };
+  environment.systemPackages = with pkgs; [
+    kompose
+    kubectl
+    kubernetes
+  ];
 
   services.kubernetes = {
     roles = ["master" "node"];
-      apiserver = {
-        securePort = 8443;
-        advertiseAddress = "api.kube";
-      };
-    masterAddress = "api.kube";
+    masterAddress = "hc-ch-1.morrislan.net";
+    apiserverAddress = "https://hc-ch-1.morrislan.net:6443";
     easyCerts = true;
+    addons.dns.enable = true;
     kubelet.extraOpts = "--fail-swap-on=false";
+    apiserver = {
+      securePort = "6443";
+      advertiseAddress = "10.1.240.5";
+    };
   };
-
 }
