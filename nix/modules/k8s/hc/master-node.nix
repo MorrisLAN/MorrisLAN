47f643a0ac00419c49971fcd865b5c56959e6e07
Maxwell Morris <git@maxmorris.io>
2024-09-16 19:34:59 +0100

    test

--- nix/modules/k8s/hc/master-node.nix
+++ nix/modules/k8s/hc/master-node.nix
@@ -16,36 +16,6 @@
     calicoctl
   ];
 
-  environment.etc."cni/net.d/10-calico.conflist" = {
-    text = builtins.toJSON {
-      name = "k8s-pod-network";
-      cniVersion = "0.3.1";
-      type = "calico";
-      plugins = [
-        {
-          type = "calico";
-          log_level = "info";
-          log_file_path = "/var/log/calico/cni/cni.log";
-          datastore_type = "kubernetes";
-          mtu = 1500;
-          nodename = config.networking.hostName;
-          ipam.type = "calico-ipam";
-          policy.type = "k8s";
-          kubernetes.kubeconfig = "/etc/kubernetes/cluster-admin.kubeconfig";
-        }
-        {
-          type = "portmap";
-          capabilities.portMappings = true;
-          snat = true;
-        }
-        {
-          type = "bandwidth";
-          capabilities.bandwidth = true;
-        }
-      ];
-    };
-  };
-
   services.kubernetes = {
     roles = ["master" "node"];
     masterAddress = "hc-ch-api.morrislan.net";
@@ -56,6 +26,49 @@
       extraOpts = "--fail-swap-on=false";
       cni = {
         packages = with pkgs; [ calico-cni-plugin ];
+        config = [
+          {
+            "cniVersion" = "0.3.1";
+            "name" = "k8s-pod-network";
+            "type" = "calico";
+            "plugins" = [
+              {
+                "type" = "calico";
+                "log_level" = "info";
+                "log_file_path" = "/var/log/calico/cni/cni.log";
+                "datastore_type" = "kubernetes";
+                "mtu" = 1500;
+                "nodename" = config.networking.hostName;
+                "ipam" = {
+                  "type" = "calico-ipam";
+                };
+                "policy" = {
+                  "type" = "k8s";
+                };
+                "kubernetes" = {
+                  "kubeconfig" = "/etc/kubernetes/cluster-admin.kubeconfig";
+                };
+              }
+              {
+                "type" = "portmap";
+                "capabilities" = {
+                  "portMappings" = true;
+                };
+                "snat" = true;
+              }
+              {
+                "type" = "bandwidth";
+                "capabilities" = {
+                  "bandwidth" = true;
+                };
+              }
+            ];
+          }
+          {
+            "cniVersion" = "0.3.1";
+            "type" = "loopback";
+          }
+        ];
       };
     };
     apiserver = {
