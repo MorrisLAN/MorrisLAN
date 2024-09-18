709538917d10808d8aee69b7e4a574bbfacd4c29
Maxwell Morris <git@maxmorris.io>
2024-09-18 21:18:01 +0100

    i'll figure out calico another time lol

--- nix/modules/k8s/hc/master-node.nix
+++ nix/modules/k8s/hc/master-node.nix
@@ -4,17 +4,14 @@
   networking = {
     extraHosts = 
     ''
-      10.1.240.5 hc-ch-1.morrislan.net
       10.1.240.5 hc-ch-api.morrislan.net
+      10.1.240.5 hc-ch-1.morrislan.net
     '';
-    dhcpcd.denyInterfaces = [ "cali*" "tunl*" "vxlan.calico" ];
   };
 
   environment.systemPackages = with pkgs; [
     kompose
     kubectl
-    calicoctl
-    calico-kube-controllers
   ];
 
   services.kubernetes = {
@@ -25,10 +22,6 @@
     addons.dns.enable = true;
     kubelet = {
       extraOpts = "--fail-swap-on=false";
-      cni = {
-        packages = with pkgs; [ calico-cni-plugin ];
-        configDir = "/var/lib/cni/net.d";
-      };
     };
     clusterCidr = "10.55.0.0/16";
     apiserver = {
@@ -36,6 +29,5 @@
       securePort = 6443;
       advertiseAddress = "10.1.240.5";
     };
-    flannel.enable = false;
   };
 }
