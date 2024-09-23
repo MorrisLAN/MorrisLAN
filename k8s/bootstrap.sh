59501e320b032b111197919121d29beb18702dea
Maxwell Morris <git@maxmorris.io@>
2024-09-23 17:19:57 +0100

    improvements

--- k8s/bootstrap.sh
+++ k8s/bootstrap.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+REPO_DIR="/tmp/morrislan"
+rm -r ${REPO_DIR}
+nix-shell -p git --command "git clone https://git.morrislan.net ${REPO_DIR}"
+
 if [[ $HOSTNAME =~ ^hc-ch-[0-9]+$ ]]; then
   kubectl create namespace argocd
-  kubectl apply -n argocd -f argocd -f argocd/hc
+  kubectl apply -n argocd -f ${REPO_DIR}/k8s/argocd -f ${REPO_DIR}/k8s/argocd/hc
   sleep 1m
-  kubectl apply -n argocd -f argocd -f argocd/hc
+  kubectl apply -n argocd -f ${REPO_DIR}/k8s/argocd -f ${REPO_DIR}/k8s/argocd/hc
 fi
