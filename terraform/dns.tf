c6e23025119d4ff15ab139cdd0712a11eff83983
Maxwell Morris <git@maxmorris.io>
2024-10-09 13:52:42 +0100

    fix

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -2,7 +2,7 @@ resource "cloudflare_record" "github_verify" {
   zone_id         = var.cloudflare_zone_id
   name            = "_github-challenge-MorrisLAN-org.morrislan.net"
   comment         = "Domain ownership verification for GitHub"
-  value           = "e2628155b4"
+  content           = "e2628155b4"
   allow_overwrite = true
   type            = "TXT"
 }
