42d394f7025fd89749c10fb44fb6a11162c83a4c
Maxwell Morris <git@maxmorris.io>
2024-10-09 02:02:44 +0100

    test

--- terraform/vapor/digitalocean_kubernetes_cluster.tf
+++ terraform/vapor/digitalocean_kubernetes_cluster.tf
@@ -1,7 +1,7 @@
 resource "digitalocean_kubernetes_cluster" "vapor" {
   name    = "vapor"
   region  = "lon1"
-  version = "1.31.1-do.2"
+  version = "1.29.9-do.2"
   node_pool {
     name       = "vapor-node"
     size       = "s-2vcpu-2gb"
