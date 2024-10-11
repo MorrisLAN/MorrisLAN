6815248d82161b51fabf609ac4aedb64942bfc84
Maxwell Morris <git@maxmorris.io>
2024-10-11 19:59:24 +0100

    Updated for RKE clusters

--- tools/k8s/cleanup-namespace.sh
+++ tools/k8s/cleanup-namespace.sh
@@ -2,20 +2,7 @@
 
 NAMESPACE=$1
 
-if [ -z "${NAMESPACE}" ]; then
-  echo "Usage: $0 <namespace>"
-  exit 1
-fi
-
-# Dump namespace JSON
-kubectl get namespace ${NAMESPACE} -o json > ${NAMESPACE}-namespace.json
-
-# Remove finalizers
-jq '.spec.finalizers = []' ${NAMESPACE}-namespace.json > ${NAMESPACE}-namespace-temp.json
-
-# Apply updated JSON
-kubectl replace --raw "/api/v1/namespaces/${NAMESPACE}/finalize" -f ${NAMESPACE}-namespace-temp.json
-
-# Clean up
-rm ${NAMESPACE}-namespace.json ${NAMESPACE}-namespace-temp.json
-kubectl get namespace ${NAMESPACE}
+kubectl get namespace "$NAMESPACE" -o json \
+  | tr -d "\n" \
+  | sed "s/\"finalizers\": \[[^]]\+\]/\"finalizers\": []/" \
+  | kubectl replace --raw "/api/v1/namespaces/$NAMESPACE/finalize" -f -
