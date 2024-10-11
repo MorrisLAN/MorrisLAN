f894fe1443db7d7a6636682bf48cd280531a6b77
Maxwell Morris <git@maxmorris.io>
2024-10-11 23:23:34 +0100

    Add Vault SSH helper tool

---
+++ tools/vault-ssh.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Helper script for SSHing to nodes via Vault
+if [ -z "$1" ]; then
+    echo "Usage: vssh <host>"
+    return 1
+fi
+
+VAULT_ADDR="https://vault.morrislan.net"
+HOST="$1"
+CLUSTER="${HOST%%-node-*}"
+
+vault token lookup >/dev/null 2>&1
+if [ $? -ne 0 ]; then
+    echo "Attempting to log in with OIDC..."
+    vault login --method oidc
+fi
+vault ssh -mode ca -role ssh -mount-point ${CLUSTER}-ssh ubuntu@${HOST} -o IdentityAgent=null -o StrictHostKeyChecking=no
