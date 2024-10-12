0b487ca409403b5ebdf46b66f84ee17ad03eb6d6
Maxwell Morris <git@maxmorris.io>
2024-10-12 01:14:08 +0100

    things

--- tools/vault-ssh.sh
+++ tools/vault-ssh.sh
@@ -7,11 +7,20 @@ fi
 
 VAULT_ADDR="https://vault.morrislan.net"
 HOST="$1"
-CLUSTER="${HOST%%-node-*}"
+
+# This is a naming mistake I am now paying for
+if [[ ${HOST} == metal-* ]]; then
+    CLUSTER="metal"
+    USER="rancher"
+else
+    CLUSTER="${HOST%%-node-*}"
+    USER="ubuntu"
+fi
 
 vault token lookup >/dev/null 2>&1
 if [ $? -ne 0 ]; then
     echo "Attempting to log in with OIDC..."
     vault login --method oidc
 fi
-vault ssh -strict-host-key-checking=no -mode ca -role ssh -mount-point ${CLUSTER}-ssh ubuntu@${HOST} -o IdentityAgent=null
+
+vault ssh -strict-host-key-checking=no -mode ca -role ssh -mount-point ${CLUSTER}-ssh ${USER}@${HOST} -o IdentityAgent=null -t "sudo -i"
