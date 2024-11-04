c496a769fa456c566a6a883441c848c5187836d5
Maxwell Morris <git@maxmorris.io>
2024-11-04 16:19:33 +0000

    fixie poo

--- tools/k8s/cleanup-namespace.sh
+++ tools/k8s/cleanup-namespace.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 # Removes finalizers from stubborn namespaces that won't delete
-
 NAMESPACE=$1
 
 kubectl get namespace "$NAMESPACE" -o json \
   | tr -d "\n" \
   | sed "s/\"finalizers\": \[[^]]\+\]/\"finalizers\": []/" \
-  | kubectl replace --raw "/api/v1/namespace/$NAMESPACE/finalize" -f -
+  | kubectl replace --raw "/api/v1/namespaces/$NAMESPACE/finalize" -f -
