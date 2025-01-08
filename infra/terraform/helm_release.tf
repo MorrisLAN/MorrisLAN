f6947778224028629404caa26930c7f3ef0f8577
Maxwell Morris <max@morrislan.net>
2025-01-08 13:48:41 +0000

    gurrrrrrr

--- infra/terraform/helm_release.tf
+++ infra/terraform/helm_release.tf
@@ -1,28 +1,28 @@
 resource "helm_release" "gitlab" {
- depends_on = [digitalocean_kubernetes_cluster.mgmt]
- name       = "gitlab"
- repository = "http://charts.gitlab.io/"
- chart      = "gitlab"
- version    = "8.7.3"
+  depends_on = [digitalocean_kubernetes_cluster.mgmt]
+  name       = "gitlab"
+  repository = "http://charts.gitlab.io/"
+  chart      = "gitlab"
+  version    = "8.7.3"
 
- namespace = "gitlab"
+  namespace = "gitlab"
 
- create_namespace = true
+  create_namespace = true
 
- set {
-   name  = "global.hosts.domain"
-   value = "gitlab.morrislan.net"
- }
- set {
-   name  = "certmanager.install"
-   value = "false"
- }
- set {
-   name  = "certmanager.installCRDs"
-   value = "false"
- }
- set {
-   name  = "nginx-ingress.enabled"
-   value = "false"
- }
+  set {
+    name  = "global.hosts.domain"
+    value = "gitlab.morrislan.net"
+  }
+  set {
+    name  = "certmanager.install"
+    value = "false"
+  }
+  set {
+    name  = "certmanager.installCRDs"
+    value = "false"
+  }
+  set {
+    name  = "nginx-ingress.enabled"
+    value = "false"
+  }
 }
