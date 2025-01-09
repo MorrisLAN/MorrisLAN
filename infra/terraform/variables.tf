702933b608deb83db30636a523a9351557566f4b
Maxwell Morris <max@morrislan.net>
2025-01-09 22:27:55 +0000

    fix

--- infra/terraform/variables.tf
+++ infra/terraform/variables.tf
@@ -20,33 +20,3 @@ variable "do_token" {
   type        = string
   sensitive   = true
 }
-
-variable "hoopdev_db_pass" {
-  description = "Password for hoop.devs DB"
-  type        = string
-  sensitive   = true
-}
-
-variable "hoopdev_oidc_id" {
-  description = "OIDC client ID for hoop.dev"
-  type        = string
-  sensitive   = true
-}
-
-variable "hoopdev_oidc_secret" {
-  description = "OIDC client secret for hoop.dev"
-  type        = string
-  sensitive   = true
-}
-
-variable "hoopdev_slack_token" {
-  description = "SlackBot token for hoop.dev"
-  type        = string
-  sensitive   = true
-}
-
-variable "cloudflare_access_tunnel_mgmt_token" {
-  description = "Token for the mgmt clusters Cloudflare Access tunnel"
-  type        = string
-  sensitive   = true
-}
