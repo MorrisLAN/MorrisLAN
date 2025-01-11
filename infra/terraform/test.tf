17a75cc8af0778543d355c252541649b2518efe3
Maxwell Morris <max@morrislan.net>
2025-01-11 02:54:52 +0000

    agghahg

--- infra/terraform/test.tf
+++ infra/terraform/test.tf
@@ -1,6 +1,6 @@
 resource "digitalocean_spaces_bucket" "bucket" {
   name   = "name-of-bucket7369"
   region = "fra1" # https://docs.digitalocean.com/products/spaces/details/availability/
-  versioning {enabled = "true"}
+  versioning { enabled = "true" }
   acl = "private"
 }
