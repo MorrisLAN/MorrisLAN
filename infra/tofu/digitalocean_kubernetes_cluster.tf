47f8a1b9b0d0af9358311728fced4b87f8276cc6
Maxwell Morris <max@morrislan.net>
2025-01-26 10:22:56 +0000

    things

--- infra/tofu/digitalocean_kubernetes_cluster.tf
+++ infra/tofu/digitalocean_kubernetes_cluster.tf
@@ -9,18 +9,3 @@ resource "digitalocean_kubernetes_cluster" "mgmt" {
     node_count = 2
   }
 }
-
-resource "digitalocean_kubernetes_node_pool" "mgmt_metrics" {
-  name       = "metrics-node"
-  cluster_id = digitalocean_kubernetes_cluster.mgmt.id
-  size       = "s-4vcpu-8gb"
-  node_count = 2
-  taint {
-    key    = "workloadKind"
-    value  = "metrics"
-    effect = "NoSchedule"
-  }
-  labels = {
-    workloadKind  = "metrics"
-  }
-}
