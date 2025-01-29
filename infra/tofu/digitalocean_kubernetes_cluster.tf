bcf06ba7da6f19afc440e8461e92c21f4a996bf0
Maxwell Morris <max@morrislan.net>
2025-01-29 13:23:17 +0000

    things

--- infra/tofu/digitalocean_kubernetes_cluster.tf
+++ infra/tofu/digitalocean_kubernetes_cluster.tf
@@ -1,10 +1,10 @@
-resource "digitalocean_kubernetes_cluster" "mgmt" {
-  name    = "mgmt"
+resource "digitalocean_kubernetes_cluster" "core" {
+  name    = "core"
   region  = "lon1"
   version = "1.31.1-do.5"
 
   node_pool {
-    name       = "mgmt-node"
+    name       = "core-node"
     size       = "s-2vcpu-4gb"
     node_count = 2
   }
