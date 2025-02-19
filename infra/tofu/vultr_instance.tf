3369c277c79cd7eef1dda8c43595ce020d22452e
Maxwell Morris <max@morrislan.net>
2025-02-19 22:10:27 +0000

    double woops

--- infra/tofu/vultr_instance.tf
+++ infra/tofu/vultr_instance.tf
@@ -1,7 +1,7 @@
 resource "vultr_instance" "kmn_uk_lon_1_prod" {
   count    = 3
   hostname = "kmn-${count.index + 1}.prod.uk-lon-1.morrislan.net"
-  plan     = "vhf-2c-2gb"
+  plan     = "vc2-2c-4gb"
   region   = "lhr"
   os_id    = 2284
   backups  = "enabled"
